9d99c0e20bb212aeb63f00507010156df7b5b9e8
Reid Weber <reid@meshstudio.io>
2018-05-09 11:41:30 -0700

    Added Tab Bar and Dummy Components

---
+++ navigation/constants.ts
@@ -0,0 +1,34 @@
+/**
+ * This interface defines all of the colors and
+ * styles we'll need for our TabNavigators and
+ * StackNavigators 
+ */
+interface NavigationStyle {
+  tabBarBackgroundColor: string
+  tabBarButtonColor: string
+  tabBarSelectedButtonColor: string
+  tabFontFamily: string
+  navBarButtonColor: string
+  navBarTextColor: string
+  navigationBarColor: string
+  navBarBackgroundColor: string
+  statusBarColor: string
+  bottomTabBadgeTextColor: string
+  bottomTabBadgeBackgroundColor: string
+}
+
+const styleConstants: NavigationStyle = {
+  tabBarBackgroundColor: '#FFFFFF',
+  tabBarButtonColor: '#A5A6A4',
+  tabBarSelectedButtonColor: '#242420',
+  tabFontFamily: 'BioRhyme-Bold',
+  navBarButtonColor: '#A5A6A4',
+  navBarTextColor: '#242420',
+  navigationBarColor: '#A5A6A4',
+  navBarBackgroundColor: '#A5A6A4',
+  statusBarColor: '#002b4c',
+  bottomTabBadgeTextColor: '#242420',
+  bottomTabBadgeBackgroundColor: '#242420'
+}
+
+export default styleConstants
