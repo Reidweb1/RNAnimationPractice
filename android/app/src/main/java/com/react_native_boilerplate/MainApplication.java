2c1dbcc8acd13ff1264c16ae46d9fd9769775a7d
Reid Weber <reid@meshstudio.io>
2018-05-11 11:42:20 -0700

    Fixing local circleCI tests

--- android/app/src/main/java/com/react_native_boilerplate/MainApplication.java
+++ android/app/src/main/java/com/react_native_boilerplate/MainApplication.java
@@ -3,6 +3,7 @@ package com.react_native_boilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,7 +24,8 @@ public class MainApplication extends NavigationApplication {
         // Add additional packages you require here
         // No need to add RnnPackage and MainReactPackage
         return Arrays.<ReactPackage>asList(
-                new MainReactPackage()
+                new MainReactPackage(),
+            new RealmReactPackage()
         );
     }
 
